0dfd18f720af704b04baa743079d8db19e924bda
Jozue <jozue.roman@gmail.com>
2010-09-27 02:25:03 -0500

    all files

---
+++ Classes/Assignment_1AAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  Assignment_1AAppDelegate.h
+//  Assignment 1A
+//
+//  Created by Josue Roman on 9/20/10.
+//  Copyright UT Austin 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Assignment_1AAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment 1A
+//
+//  Created by Josue Roman on 9/20/10.
+//  Copyright UT Austin 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
